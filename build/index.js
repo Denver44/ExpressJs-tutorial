18e2d96b0df45e131a6e52417dfaef024f7ffdc5
Denver <70090559+Denver44@users.noreply.github.com>
2021-12-26 17:39:38 +0530

    Basic route with express

--- build/index.js
+++ build/index.js
@@ -1,2 +1,14 @@
 "use strict";
-console.log('hi there');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+var PORT = 3000;
+app.get('/', function (req, res) {
+    res.send("\n    <h1> Hi there!</h1>");
+});
+app.listen(PORT, function () {
+    console.log('Listening on Port ', PORT);
+});
